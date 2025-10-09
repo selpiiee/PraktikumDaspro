0cbb13faf0b5f19e0843b4daad5fa0b8b3689d1f
“selpiiee” <selvynurkhoffifa@gmail.com>
2025-10-09 11:49:11 +0700

    Attempt 1

---
+++ jobsheet5/ifprintKRS21.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class ifprintKRS21 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("---Print KRS SIAKAD---");
+        System.out.println("Have the tuition fees been paid in full? (true/false): ");
+        boolean uktPaid = sc.nextBoolean();
+
+        if (uktPaid) {
+            System.out.println("UKT payent has been verified");
+            System.out.println("KRS now can be printed and you can ask the academic advisor to sign it.");
+        }
+    }
+}
